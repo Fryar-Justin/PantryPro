d1bff799af5e70a4417558099e4defc88b662ad2
Fryar-Justin <fry06001@byui.edu>
2018-03-10 17:44:21 -0700

    Beginning the wireframe for inventory

--- PantryPro/app/src/main/java/edu/byui/pantrypro/MainActivity.java
+++ PantryPro/app/src/main/java/edu/byui/pantrypro/MainActivity.java
@@ -2,12 +2,34 @@ package edu.byui.pantrypro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String items[] = new String [] {"Apple", "Orange", "Banana", "Grapes", "Pineapple", "Car", "Door", "Airplane", "Engine", "Transformers", "Sockit", "Justin", "Wheel", "Substance", "Timmy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView shoppingList = (ListView) findViewById(R.id.shoppingList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        shoppingList.setAdapter(adapter);
+
+        shoppingList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String food = String.valueOf(adapterView.getItemAtPosition(i));
+                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }

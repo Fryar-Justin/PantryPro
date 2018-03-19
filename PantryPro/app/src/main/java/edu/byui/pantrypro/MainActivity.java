fd0103d99222e6d4daa4831152bc96ea4be248ab
Hanson-Tyler <tyler.hanson123@gmail.com>
2018-03-19 10:07:14 -0700

    adding database

--- PantryPro/app/src/main/java/edu/byui/pantrypro/MainActivity.java
+++ PantryPro/app/src/main/java/edu/byui/pantrypro/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.byui.pantrypro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,27 +10,43 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    String items[] = new String [] {"Apple", "Orange", "Banana", "Grapes", "Pineapple", "Car", "Door", "Airplane", "Engine", "Transformers", "Sockit", "Justin", "Wheel", "Substance", "Timmy"};
+    ListView shoppingList;
+    MyDBHandler dbHandler;
+    Intent shareButtonIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shoppingList = (ListView) findViewById(R.id.shoppingList);
+        dbHandler = new MyDBHandler(this, null, null, 1);
 
-        ListView shoppingList = (ListView) findViewById(R.id.shoppingList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        shoppingList.setAdapter(adapter);
 
         shoppingList.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         String food = String.valueOf(adapterView.getItemAtPosition(i));
-                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_SHORT).show();
                     }
                 }
         );
+
+        printDatabase();
+    }
+
+    public void addButtonClicked(View view){
+        shareButtonIntent = new Intent(MainActivity.this, addInventoryItem.class);
+        startActivity(shareButtonIntent);
+    }
+
+    public void printDatabase(){
+        ArrayList<String> stringList = dbHandler.populateArrayList();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, stringList );
+        shoppingList.setAdapter(arrayAdapter);
     }
 }

4349c45e398bacfef83b22181aa79d0efba54141
Fryar-Justin <fry06001@byui.edu>
2018-04-09 09:58:10 -0600

    Added feedback for when items are not added to the grocery list

--- PantryPro/app/src/main/java/edu/byui/pantrypro/RecipeDetailsActivity.java
+++ PantryPro/app/src/main/java/edu/byui/pantrypro/RecipeDetailsActivity.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -44,16 +45,31 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String name = extras.getString("NAME");
         Recipe recipe = dbHandler.findRecipe(name);
+        String rejected = "";
+        int numberAdded = 0;
 
         ArrayList<Ingredient> ingredients = recipe.getIngredients();
         for(int i = 0; i < ingredients.size(); i++){
             if(!dbHandler.checkIfExists(ingredients.get(i).getName())){
                 dbHandler.addGrocery(ingredients.get(i));
+                numberAdded++;
+            }
+            else {
+                rejected += ingredients.get(i).getName() + "\n";
             }
         }
+        if (!rejected.equals("")) {
+            rejected += "are already in your inventory and were not added to the grocery list";
+            Toast.makeText(RecipeDetailsActivity.this, rejected, Toast.LENGTH_LONG).show();
+        }
 
-        Intent grocery = new Intent(RecipeDetailsActivity.this, GroceryListActivity.class);
-        startActivity(grocery);
+        if (numberAdded != 0) {
+            Intent grocery = new Intent(RecipeDetailsActivity.this, GroceryListActivity.class);
+            startActivity(grocery);
+        }
+        else {
+            Toast.makeText(RecipeDetailsActivity.this, "All of the items you entered are already in your inventory", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void setListenersAndLabels() {
